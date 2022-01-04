cc3940688763ea03c5c24226177e70101b6b47e8
hoangminhtuan1399 <hoangminhtuan1399@gmail.com>
2022-01-05 00:03:40 +0700

    Homework

---
+++ BAI7/script.js
@@ -0,0 +1,7 @@
+alert("Bài 2");
+let firstNumber = prompt("First number?", 1);
+let secondNumber = prompt("Second number?", 2);
+alert(firstNumber + secondNumber);
+alert("Bài 3");
+let word = prompt("Nhập gì đó mà bạn thích: ");
+alert("Số kí tự: " + word.length);
